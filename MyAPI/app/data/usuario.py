aff47d355340532bc38cb78a342cf34c349e9114
IsabelNavarrete04 <123046270@upq.edu.mx>
2026-03-24 09:26:37 -0600

    arreglos de la práctica 9

--- MyAPI/app/data/usuario.py
+++ MyAPI/app/data/usuario.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Interger, String
+from sqlalchemy import Column, Integer, String
 from app.data.db import Base
 
 class usuario(Base):
     __tablename__ = "tb-usuarios"
     
-    id = Column(Interger, primary_key = True, index = True)
+    id = Column(Integer, primary_key = True, index = True)
     nombre = Column(String)
-    edad = Column(Interger)
+    edad = Column(Integer)
--- MyAPI/app/main.py
+++ MyAPI/app/main.py
@@ -1,9 +1,14 @@
 #importaciones
 from fastapi import FastAPI
 from app.router import usuario, misc
+from app.data.db import engine
+from app.data import usuario as usuarioDB
+
+usuarioDB.Base.metadata.create_all(bind = engine)
 
 #Instancoa del servidor
 app = FastAPI()
 
 app.include_router(usuario.router)
 app.include_router(misc.misc)
+
